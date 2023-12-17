4c0444d5137fbdaced416e29fff44d9cdad2d59a
Kelly Luong <kellyluong06@gmail.com>
2023-12-16 20:22:53 -0600

    Added html and css to design the game

--- rps.js
+++ rps.js
@@ -1,41 +1,50 @@
-let playerSelection = 0;
-let computerSelection = 0;
-
-function getComputerChoice () {
-    let random = Math.floor(Math.random() * 3);
-    if (random == 0) {
-        return "rock"
+//function that randomly gets the computer selection
+function getComputerChoice() {
+    let num = Math.floor((Math.random() * 3) + 1)
+    let choice = '';
+    switch (num) {
+        case 1: choice = 'rock'; break;
+        case 2: choice = 'paper'; break;
+        case 3: choice = 'scissors'; break;
     }
-    else if (random == 1) {
-        return "paper"
-    }
-    else {
-        return "scissors"
-    }
+    return choice;
 }
 
-function game () {
-
-for (i = 0; i < 6; i++) {
-
+//function that plays a round of rps
+//and adds points to appropiate side
 function playRound(playerSelection, computerSelection) {
-    let computerSelection = getComputerChoice
-    let playerScore = 0
-    let computerScore = 0
-    if (playerSelection == 'rock' && computerSelection == 'scissors' || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')) {
-        playerScore +=1
-        return "Congrats! " + playerSelection + "beats " + computerSelection + "!"
+    let pscore = 0;
+    let cscore = 0;
+    let print = '';
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') || 
+        (playerSelection == 'scissors' && computerSelection == 'paper')) {
+        
+        pscore++;
+        print = 'you win!';
+
+        if (pscore == 5 && cscore < 5) {
+            print = 'you won the game!';
+        }
     }
-    if (playerSelection == computerSelection) {
-            return "It's a tie!"
+    else if (playerSelection == computerSelection) {
+        print = 'tie. try again.';
     }
     else {
-        computerScore +=1
-        return "Aww... " + computerSelection + "beats " + playerSelection + "!"
+        cscore++;
+        print = 'you lose!';
+
+        if (cscore == 5 && pscore < 5) {
+            print = 'you lost the game...';
+        }
     }
-}
+    return print;
+  }
 
-}
-
-}
+  const playerSelection = 'rock';
+  console.log("this is player: " + playerSelection);
+  const computerSelection = getComputerChoice();
+  console.log("this is computer: " + computerSelection);
+  console.log(playRound(playerSelection, computerSelection));
     
